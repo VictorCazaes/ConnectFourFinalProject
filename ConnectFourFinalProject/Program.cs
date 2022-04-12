6a1b9d2377b1a9bad444ed78f5874b78cb7f5b6a
Victor Cazaes De Oliveira <v.cazaesdeoliveir486@mybvc.ca>
2022-04-12 10:51:00 -0600

    Add classes

--- ConnectFourFinalProject/Program.cs
+++ ConnectFourFinalProject/Program.cs
@@ -2,6 +2,31 @@
 
 namespace ConnectFourFinalProject
 {
+    public abstract class Player
+    {
+        protected string playerName;
+
+        public Player()
+        {
+            playerName = "Player";
+        }
+        public Player(string name)
+        {
+            playerName = name;
+        }
+    }
+    public abstract class Game
+    {
+
+    }
+    public class Controller
+    {
+
+    }
+    public class GameInformation
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
