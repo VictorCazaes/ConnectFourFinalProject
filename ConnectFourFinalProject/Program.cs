bdf4cf8a03c3411da7905f8836dc29fd5539b94a
Victor Cazaes De Oliveira <v.cazaesdeoliveir486@mybvc.ca>
2022-04-12 14:26:19 -0600

    Add get players information

--- ConnectFourFinalProject/Program.cs
+++ ConnectFourFinalProject/Program.cs
@@ -2,22 +2,37 @@
 
 namespace ConnectFourFinalProject
 {
-    public abstract class Player
+    public class Player
     {
-        protected string playerName;
+        protected string PlayerName;
+        protected int Wins { get; set; }
 
-        public Player()
+        public Player(string playerName, int playerNum)
         {
-            playerName = "Player";
+            if(playerName == "")
+            {
+                PlayerName = $"Player-{playerNum}";
+            } else
+            {
+                PlayerName = playerName;
+            }
         }
-        public Player(string name)
+
+        public virtual string GetPlayerName()
         {
-            playerName = name;
+            return PlayerName;
         }
     }
     public abstract class Game
     {
+        protected static void SetupANewGame()
+        {
 
+        }
+        public virtual void Reset()
+        {
+            SetupANewGame();
+        }
     }
     public class Controller
     {
@@ -31,7 +46,37 @@ namespace ConnectFourFinalProject
     {
         static void Main(string[] args)
         {
-
+            do
+            {
+                Console.WriteLine("Press enter to start or type 'exit' to exit the game");
+                string choice = Console.ReadLine();
+                if(choice == "exit")
+                {
+                    break;
+                } 
+                else if(choice == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Type the desired name for player one or press enter to use the default name");
+                    string player1Name = Console.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("Type the desired name for player two or press enter to use the default name");
+                    string player2Name = Console.ReadLine();
+                    Player test1 = new Player(player1Name, 1);
+                    Player test2 = new Player(player2Name, 2);
+                    Console.Clear();
+                    Console.WriteLine("Players:");
+                    Console.WriteLine(test1.GetPlayerName());
+                    Console.WriteLine(test2.GetPlayerName());
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Unknown command!");
+                    continue;
+                }
+            } while (true);
         }
     }
 }
