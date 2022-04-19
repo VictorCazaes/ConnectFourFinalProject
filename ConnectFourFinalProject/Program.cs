68b3a100f2f99f1c85c0b6fb72ea743b5fe5d214
gabriel427131 <g.deoliveira131@mybvc.ca>
2022-04-18 21:21:47 -0600

    Implemented LetterIncolumn, FullBoard and draft WinMethod

--- ConnectFourFinalProject/Program.cs
+++ ConnectFourFinalProject/Program.cs
@@ -23,15 +23,19 @@ namespace ConnectFourFinalProject
             return PlayerOne;
         }
     }
+
     public abstract class GameBase : Player
     {
         protected static int turn;
         private string PlayerTwo;
-        public static bool[] columnfull = new bool[6];
+        public static bool[] columnFull = new bool[numColumns];
         public static char pLetter;
         public static int columnInserted = 0;
-        protected static char[,] Board = new char[6,7];
 
+        public const int numColumns = 7;
+        public const int numRows = 6;
+        protected static char[,] Board = new char[numRows, numColumns];
+        
         public GameBase(string playerOneName, string playerTwoName) : base(playerOneName)
         {
             if (playerTwoName == "")
@@ -43,18 +47,20 @@ namespace ConnectFourFinalProject
                 PlayerTwo = playerTwoName;
             }
         }
+
         protected static void SetupANewGame()
         {
             turn = 0;
-            for(int i = 0; i < 6; i++)
+            for(int i = 0; i < numRows; i++)
             {
-                columnfull[i] = false;
-                for (int j = 0; j < 7; j++)
+                columnFull[i] = false;
+                for (int j = 0; j < numColumns; j++)
                 {
                     Board[i, j] = '*';
                 }
             }
         }
+
         public virtual void Reset()
         {
             SetupANewGame();
@@ -78,9 +84,9 @@ namespace ConnectFourFinalProject
 
         public virtual void DisplayBoard()
         {
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < numRows; i++)
             {
-                for (int j = 0; j < 7; j++)
+                for (int j = 0; j < numColumns; j++)
                 {
                     Console.Write($"{Board[i, j]} ") ;
                 }
@@ -88,6 +94,77 @@ namespace ConnectFourFinalProject
             }
         Console.WriteLine("0 1 2 3 4 5 6");
         }
+
+        public void PutLetterInColumn(int columninserted, char letter)
+        {
+            int index = numRows - 1;
+            char pl = Board[index, columninserted]; // pl for Player Letter
+            while ((pl == 'X' || pl == 'O') && index >= 0)
+            {
+                index--;
+                if (index >= 0) pl = Board[index, columninserted];
+            }
+            if (index < 0) columnFull[columninserted] = true;
+            if (!columnFull[columninserted])
+            {
+                Board[index, columninserted] = letter;
+                DisplayBoard();
+            }
+            else
+            {
+                Console.WriteLine("Column full! Choose another one.");
+            }
+        }
+
+        public bool FullBoard()
+        {
+            bool boardFull = true;
+            for (int i = 0; i < numRows;i++)
+            {
+                if (!columnFull[i])
+                {
+                    boardFull = false;
+                }
+                if (boardFull)
+                {
+                    DisplayBoard();
+                }
+            }
+            return boardFull;
+        }
+
+        public static bool WinMethod(char letter)
+        {
+            bool winnerBool = false;
+
+            //Horizontal lines
+            for(int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Board[i, j] == letter && Board[i, j + 1] == letter && Board[i, j + 2] == letter && Board[i, j + 3] == letter)
+                    {
+                        winnerBool = true;
+                    }
+                }
+            }
+
+            //Vertical lines
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0;j < 7; j++)
+                {
+                    if(Board[i, j] == letter && Board[i + 1, j] == letter && Board[i + 2, j] == letter && Board[i + 3, j] == letter)
+                    {
+                        winnerBool = true;
+                    }
+                }
+            }
+
+            return winnerBool;
+        }
+
+
     }
     public class GameController : GameBase
     {
@@ -98,42 +175,62 @@ namespace ConnectFourFinalProject
 
         public override void Play()
         {
-            //if(GetPlayerName(true) == PlayerOne)
-            //{
-            //    pLetter = 'X';
-            //}
-            //else
-            //{
-            //    pLetter = 'O';
-            //}
+            if (GetPlayerName(true) == PlayerOne)
+            {
+                pLetter = 'X';
+            }
+            else
+            {
+                pLetter = 'O';
+            }
 
-            //do
-            //{
-            //    string numInsert = Console.ReadLine();
-            //    columnInserted = Convert.ToInt32(numInsert);
+            do
+            {
+                string numInsert = Console.ReadLine();
+                columnInserted = Convert.ToInt32(numInsert);
 
-            //    //double numInsert = Double.Parse(Console.ReadLine());
-            //    //columnInserted = Convert.ToInt32(numInsert);
+                if (columnInserted >= 0 && columnInserted <= 7)
+                {
 
-            //    if (columnInserted >= 0 && columnInserted <= 8)
-            //    {
+                    PutLetterInColumn(columnInserted, pLetter);
 
-            //        //putLetterInColumn(columnInserted, pLeter)    Need to do it
-            //        if (!columnfull[columnInserted])
-            //        {
-            //            pLetter = (pLetter == 'O' ? 'X' : 'O');
-            //        }
-            //        else if (WinMethod)    Need to do this method...?
-            //        {
-            //            Console.WriteLine("Player name '{0}' won!", );
-            //        }
+                    if (!columnFull[columnInserted])
+                    {
+                        pLetter = (pLetter == 'O' ? 'X' : 'O');
+                    }
+                    else if (WinMethod(pLetter)) // redundant method...?
+                    {
+                        Console.WriteLine("Player '{0}', with '{1}' symbol won the game!", base.GetPlayerName(), pLetter);
+                        SetupANewGame();
+                    }
 
-            //    }
+                    if (FullBoard())
+                    {
+                        Console.WriteLine("It is a draw! Start the games again.");
+                        SetupANewGame();
+                    }
 
-            //} while (true);
+                }
 
-            ////Board[5, 0] = 'A';
+            } while (true);
+
+
+
+            //Board[5, 0] = 'A';
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public override string GetPlayerName(bool win)
         {
@@ -173,11 +270,9 @@ namespace ConnectFourFinalProject
                     Console.WriteLine(newGame.GetPlayerName(false));
                     newGame.DisplayBoard();
                     newGame.Play();
-                    Console.Clear();
-                    newGame.DisplayBoard();
-                    Console.ReadKey();
-                    
+                    //Console.Clear();
                     //newGame.DisplayBoard();
+                    //Console.ReadKey();
 
                 }
                 else
